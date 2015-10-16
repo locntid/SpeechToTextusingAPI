5dcff9874e1648b7d716fbaab03efac2ae4cbcaa
locnt.ssj <locnt.ssj@gmail.com>
2015-10-16 11:43:51 +0700

    locnt
    Change

--- app/src/main/java/info/tutsmodel/speechtotextusingapi/MainActivity.java
+++ app/src/main/java/info/tutsmodel/speechtotextusingapi/MainActivity.java
@@ -1,30 +1,31 @@
 package info.tutsmodel.speechtotextusingapi;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     protected static final int RESULT_SPEECH = 1;
 
     private ImageButton btnSpeak;
-    private TextView txtText;
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtText = (TextView) findViewById(R.id.txtText);
+        listView = (ListView) findViewById(R.id.listView);
 
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
 
@@ -36,11 +37,11 @@ public class MainActivity extends Activity {
                 Intent intent = new Intent(
                         RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 
                 try {
                     startActivityForResult(intent, RESULT_SPEECH);
-                    txtText.setText("");
                 } catch (ActivityNotFoundException a) {
                     Toast t = Toast.makeText(getApplicationContext(),
                             "Opps! Your device doesn't support Speech to Text",
@@ -59,11 +60,11 @@ public class MainActivity extends Activity {
         switch (requestCode) {
             case RESULT_SPEECH: {
                 if (resultCode == RESULT_OK && null != data) {
-
+                    // Dữ liệu trả về là một mảng String
                     ArrayList<String> text = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-                    txtText.setText(text.get(0));
+                    listView.setAdapter(new ArrayAdapter<>(MainActivity.this,
+                            android.R.layout.simple_list_item_1, text));
                 }
                 break;
             }
